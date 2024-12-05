acb7931859f1bb883dacc638b4591e86a4a096f5
mari <marigorsamuel@gmail.com>
2024-12-05 16:24:15 -0300

    feat: ensure barbershops can be searched by title and service

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -20,7 +20,7 @@ import {
 } from "./ui/form"
 
 const formSchema = z.object({
-  search: z.string().trim().min(1, {
+  title: z.string().trim().min(1, {
     message: "Digite algo para buscar",
   }),
 })
@@ -29,13 +29,13 @@ const Search = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      search: "",
+      title: "",
     },
   })
   const router = useRouter()
 
   const handleSubmit = (data: z.infer<typeof formSchema>) => {
-    router.push(`/barbershops?search=${data.search}`)
+    router.push(`/barbershops?title=${data.title}`)
   }
 
   return (
@@ -43,7 +43,7 @@ const Search = () => {
       <form onSubmit={form.handleSubmit(handleSubmit)} className="flex gap-2">
         <FormField
           control={form.control}
-          name="search"
+          name="title"
           render={({ field }) => (
             <FormItem className="w-full">
               <FormControl>
