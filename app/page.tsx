d2f618cc8bcf98dc51ac43c644f2d8886290e474
mari <marigorsamuel@gmail.com>
2024-12-10 10:39:34 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -45,8 +47,14 @@ const Home = async () => {
       <Header />
       <div className="p-5">
         {/* TEXTO */}
-        <h2 className="text-xl font-bold">Olá, Igor!</h2>
-        <p>Sabado, 30 de novembro.</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "bem vindo"}!
+        </h2>
+        <span className="capitalize">
+          {format(new Date(), "EEE, dd ", { locale: ptBR })}
+        </span>
+        <span>&nbsp;de&nbsp;</span>
+        <span>{format(new Date(), "MMMM", { locale: ptBR })}</span>
 
         {/* BUSCA */}
         <div className="mt-6">
